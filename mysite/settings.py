4a432c7ebf8b941687b4a06802b5e972cba6e7d4
gerry <gerasimoskounadis@gmail.com>
2021-06-19 19:13:32 +0300

    static root

--- mysite/settings.py
+++ mysite/settings.py
@@ -117,11 +117,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/css')
 #STATIC_ROOT = '/home/gerrykoun/gerrykoun.pythonanywhere.com/static' #for running on python anywhere
 STATIC_URL = '/static/'
-
-#STATICFILES_DIRS =os.path.join(BASE_DIR, 'static'),
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
